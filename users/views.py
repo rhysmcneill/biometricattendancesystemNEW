f5407c25bdc370621af88552b3ac7aaf4bc5789b
rhysmcneill <33672728+rhysmcneill@users.noreply.github.com>
2022-02-11 08:36:38 +0000

    Facial recognition w/ update db

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 import os
 import pickle
 from datetime import datetime, time
+from pyexpat import model
+
 import dlib
 import face_recognition
 import numpy as np
 from imutils import face_utils
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from django.views.decorators.http import condition
 from sklearn.svm import SVC
@@ -22,15 +25,15 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 import cv2
 import imutils
-from imutils.face_utils import FaceAligner
-import matplotlib
-# Create your views here.
+from users.models import is_Present, clocked_Time
+
 
 def username_present(username):
     if User.objects.filter(username=username).exists():
         return True
 
     return False
+
 
 # @register(request) takes a form request and ensures it is POST method
 @login_required
@@ -45,6 +48,7 @@ def register(request):
     else:
         form = UserRegForm()
     return render(request, "users/register.html", {'form': form })
+
 
 def loginUser(request):
     if request.method == 'POST':
@@ -61,6 +65,8 @@ def loginUser(request):
                 return redirect('/howitworks')
     else:
         return render(request, 'users/login.html', {})
+
+
 # Login required to access employee dashboard
 @login_required
 def dashboard(request):
@@ -75,6 +81,7 @@ def loading_training(request):
 def admindashboard(request):
     return render(request, 'users/adminDashboard.html')
 
+
 def logout_view(request):
     logout(request)
     return redirect('home')
@@ -83,11 +90,12 @@ def logout_view(request):
 def display_livefeed(request):
     return render(request, 'users/vidStream.html')
 
+
 def create_dataset(username):
     id = username
-    if (os.path.exists('image_data/Train_Data/{}/'.format(id)) == False):
-        os.makedirs('image_data/Train_Data/{}/'.format(id))
-    directory = 'image_data/Train_Data/{}/'.format(id)
+    if (os.path.exists('image_data\Train_Data\{}\\'.format(id)) == False):
+        os.makedirs('image_data\Train_Data\{}\\'.format(id))
+    directory = 'image_data\Train_Data\{}\\'.format(id)
 
     # Detect face
     # Load HOG
@@ -174,31 +182,19 @@ def add_photos(request):
         return render(request, 'dashboard')
 
 
-def data_points(data_points, all_person_names):
-    all_data_points = TSNE(n_components=2,
-                      learning_rate='auto').fit_transform(data_points)
-
-    for i, names in enumerate(set(all_person_names)):
-        idx = all_person_names == names
-        plt.scatter(all_data_points[idx, 0], all_data_points[idx, 1], label=names)
-
-    plt.legend(bbox_to_anchor=(1, 1))
-    plt.title('Scatter graph for employee facial data', fontdict=None, loc='center',)
-    matplotlib.rcParams.update({'figure.autolayout': True})
-    plt.tight_layout()
-    plt.savefig('users/static/users/images/training_visualisation.png')
-    plt.close()
-
-
 def train(request):
 
     # Dataset directory for training.
-    dataset_directory = 'image_data/Train_Data'
+    dataset_directory = 'image_data\Train_Data'
 
     person_counter = 0
     face_encodings = []
     person_names = []
     counter = 0
+    label_id = 0
+    images = []
+    labels = []
+    names = {}
 
     # Checks for amount of personnel in dataset and their images
     for person_name in os.listdir(dataset_directory):
@@ -211,37 +207,36 @@ def train(request):
     # Checks and joins person_name to directory and encodes a 128-dimension face encoding for each face in the image.
     for person_name in os.listdir(dataset_directory):
         print(str(person_name))
+        names[label_id] = person_names
         current_directory = os.path.join(dataset_directory, person_name)
         if not os.path.isdir(current_directory):
             continue
         for img in image_files_in_folder(current_directory):
             print(str(img))
             read_image = cv2.imread(img)
+            label = label_id
+            images.append(cv2.imread(img, 0))
+            labels.append(int(label)  )
             try:
                 # 128-dimension encoding
                 face_encodings.append((face_recognition.face_encodings(read_image)[0]).tolist())
 
                 person_names.append(person_name) # Appending names to list
                 counter += 1
+
             except:
                 print("Image Removed from Directory")
                 os.remove(img)
+        label_id += 1
 
-    # Add all dataset names to a NumPy array
-    all_person_names = np.array(person_names)
-    le = LabelEncoder() # Label encoder
-    le.fit(person_names)
-    normalised_encoding = le.transform(person_names)
-    all_face_encodings = np.array(face_encodings)
-    np.save('image_data/classification.npy', le.classes_)
-    # Using linear support vector classification to split data into classes and train the model
-    support_vector_class = SVC(kernel='linear', probability=True)
-    support_vector_class.fit(all_face_encodings, normalised_encoding)
-    support_vector_class_directory = "image_data/support_vector_class.sav"
-    with open(support_vector_class_directory, 'wb') as file:
-        pickle.dump(support_vector_class, file)
+    # Preparing data for trainging
+    (images, labels) = [np.array(lis) for lis in [images, labels]]
 
-    data_points(all_face_encodings, all_person_names)
+    # training the model
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.train(images, labels)
+    model.save(r'mainsite\\templates\mainsite\\trainningData.yml')
+
 
 @login_required
 def training_dataset(request):
@@ -256,91 +251,111 @@ def training_dataset(request):
         messages.warning(request, f'Error - please see logs for details.')
         return render(request, 'admindashboard')
 
-@login_required
+
 def face_recognition_mark_in(request):
-
-    # Detect face by loading HOG
-    print("[INFO] Loading the facial detector")
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor('mainsite/templates/mainsite/shape_predictor_68_face_landmarks.dat')
-    support_vector_class_directory = "image_data/support_vector_class.sav"
-
-    with open(support_vector_class_directory, 'rb') as file:
-        support_vector_class = pickle.load(file)
-    fa = FaceAligner(predictor, desiredFaceWidth=100)
-    le = LabelEncoder()
-    le.classes_ = np.load('image_data/classification.npy')
-    encodings = np.zeros((1, 128))
-    all_faces = len(support_vector_class.predict_proba(encodings)[0])
-
+    # Dataset directory
+    dataset_directory = 'image_data\Train_Data'
+    person_counter = 0
+    id = 0
+    name = {}
     count = dict()
     is_present = dict()
     current_time = dict()
-    start = dict()
-    for i in range(all_faces):
-        count[le.inverse_transform([i])[0]] = 0
-        is_present[le.inverse_transform([i])[0]] = False
 
-    # Initialise cv2 video stream
-    print("[INFO] Initializing Video stream")
-    video = cv2.VideoCapture(0)
+    # Gets all the personnel within the system
+    for person_name in os.listdir(dataset_directory):
+        current_directory = os.path.join(dataset_directory, person_name)
+        name[id] = person_name
+        if not os.path.isdir(current_directory):
+            continue
+        for img in image_files_in_folder(current_directory):
+            person_counter += 1
+        id += 1
 
+    # Gets the trained dataset and initialises the face recogniser/detector
+    # We use Local Binary Patterns Histogram algorithm to recognise faces in the feed
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.read(r'mainsite\\templates\mainsite\\trainningData.yml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Capturing the faces one by one and detect the faces and showing it on the window
-    while (True):
-        # Capturing the image
-        # video.read each frame
-        _, img = video.read()
-        # Resize each image
-        img = imutils.resize(img, width=1000)
-        # convert img to grey for the ML Classifier to work
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # This will detect all the images in the current frame, and it will return the coordinates of the faces
-        # Takes in image and some other parameter for accurate result
-        faces = detector(gray, 0)
-        # @faces - this ensures there can be multiple faces so we have to get each and every face and draw a rectangle around its
+    # Initialise camera
+    webcam = cv2.VideoCapture(0)
+    while True:
+        (_, frame) = webcam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        for face in faces:
-            print("INFO : inside for loop")
-            (x, y, w, h) = face_utils.rect_to_bb(face)
+        for (x, y, w, h) in faces:
+            for employee in dataset_directory:
+                face = gray[y:y + h, x:x + w]
+                face_resize = cv2.resize(face, (130, 100))
+                # Predicting faces based on model
+                prediction = model.predict(face_resize)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                # If prediction < 80 then dont mark employee in - < 80% is not valid
+                if prediction[1] < 80:
+                    count[employee] = 0
+                # If prediction <= 120 and > 80 then mark employee in - > 80% is valid
+                elif prediction[0] <= 120:
+                    cv2.putText(frame, '% s - %.0f' %
+                                (name[prediction[0]], prediction[1]), (x - 10, y - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+                    is_present[employee] = True
+                    current_time[employee] = datetime.now()
+                    count[employee] = count.get(employee, 0) + 1
+                else:
+                    cv2.putText(frame, 'Unknown Entity',
+                                (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
 
-            face_aligned = fa.align(img, gray, face)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        cv2.imshow('Facial Recogniser - Attendance (Press q to quit)', frame)
 
-            (prediction, probability) = predict(face_aligned, support_vector_class)
-
-            if (prediction != [-1]):
-
-                    employee_name = le.inverse_transform(np.ravel([prediction]))[0] # Turns labels into original encoding
-                    prediction = employee_name
-                    if count[prediction] == 0:
-                        start[prediction] = time.time()
-                        count[prediction] = count.get(prediction, 0) + 1
-
-                    if count[prediction] == 4 and (time.time() - start[prediction]) > 1.2:
-                        count[prediction] = 0
-                    else:
-                        is_present[prediction] = True
-                        current_time[prediction] = datetime.datetime.now()
-                        count[prediction] = count.get(prediction, 0) + 1
-                        print(prediction, is_present[prediction], count[prediction])
-                    cv2.putText(img, str(employee_name) + str(probability), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-            else:
-                employee_name = "unknown"
-                cv2.putText(img, str(employee_name), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-        # Showing the image in another window
-        # Creates a window with window name "Mark Attendance - In - Press q to exit" and with the image img
-        cv2.imshow("Mark Attendance - In - Press q to exit", img)
-        # Before closing it we need to give a wait command, otherwise the open cv wont work
-        # @params with the millisecond of delay 1
-        # cv2.waitKey(1)
-        # To get out of the loop
         key = cv2.waitKey(50) & 0xFF
         if (key == ord("q")):
             break
 
-    # Stoping the videostream
-    # destroying all the windows
+    webcam.release()
     cv2.destroyAllWindows()
+    update_db_attendance_in(is_present)  # update db with attendance
+
+
+@login_required
+def mark_attendance_in(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, f'User not authorised')
+        return redirect('home')
+    elif request.user.is_superuser or request.user.is_staff:
+        messages.warning(request, f'Admin cannot use this functionality')
+        return redirect('home')
+    elif request.user.is_authenticated:
+        face_recognition_mark_in(request)
+        messages.success(request, f'Attendance successful')
+        return redirect('home')
+    else:
+        messages.warning(request, f'Error - please see logs for details.')
+        return redirect(request, 'home')
+
+
+def update_db_attendance_in(present):
+    today = datetime.today()
+    time = datetime.now()
+    for employee in present:
+        user = User.objects.get(username=employee)
+        try:
+            attendance = is_Present.objects.get(user=user, date=today)
+        except:
+            attendance = None
+
+        if attendance is None:
+            if present[employee] == True:
+                a = is_Present(user=user, date=today, present=True)
+                a.save()
+            else:
+                a = is_Present(user=user, date=today, present=False)
+                a.save()
+        else:
+            if present[employee] == True:
+                attendance.present = True
+                attendance.save(update_fields=['present'])
+        if present[employee] == True:
+            a = clocked_Time(user=user, date=today, time=time, out=False)
+            a.save()
